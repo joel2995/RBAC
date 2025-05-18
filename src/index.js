b847205489063a0a03746ca488cce193d0388448
joel2995 <joelsundarsingh2005@gmail.com>
2025-05-18 17:57:03 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const dbConnect = require("./config/dbConnect")
+
+
+
+dbConnect();
+const app = express();
+
+//Middleware
+
+app.use(express.json());
+
+//Routes
+
+//Start the server
+
+const PORT = process.env.PORT || 7002;
+app.listen(PORT , () =>{
+    console.log(`Server is running at port ${PORT}`);
+});
